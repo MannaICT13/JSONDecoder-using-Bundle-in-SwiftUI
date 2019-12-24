efee5fb57043845a8ab05c543260d78d758c8472
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-12-25 00:34:27 +0600

    Initial Commit

---
+++ JSONDecoder using Bundle in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONDecoder using Bundle in SwiftUI
+//
+//  Created by Md Khaled Hasan Manna on 25/12/19.
+//  Copyright © 2019 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
